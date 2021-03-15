2528a40e030cb56d1ff603677125586d10549f00
Abhi Jain <devabhixda@gmail.com>
2021-03-15 11:36:03 +0530

    Uprev
    Signed-off-by: Abhi Jain <devabhixda@gmail.com>

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -12,9 +12,9 @@ const RESOURCES = {
 "favicon.png": "f98a586304bc9b19e98088841e9bd944",
 "icons/Icon-192.png": "0183437af7544b2f0beebf3d22a1b455",
 "icons/Icon-512.png": "5b2a014335cfd001fa47b6b1ef4b7aab",
-"index.html": "690292e01319265f0eeaccb96cb2857e",
-"/": "690292e01319265f0eeaccb96cb2857e",
-"main.dart.js": "67c12b30625fc05af9d2a994622caab7",
+"index.html": "f73fbd68ae1b67ed378f742ab7d1f86a",
+"/": "f73fbd68ae1b67ed378f742ab7d1f86a",
+"main.dart.js": "e3c5bceca3fab5fa170b99ebd097b866",
 "manifest.json": "0d56b3d07c77407da19f7e5377e59858",
 "styles.css": "1a0908f08f70f1f27d58f0351a875120",
 "version.json": "0ec2af4716e46aea8941006bdcdd6d4c"
